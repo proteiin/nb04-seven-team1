021afe09720f99c22a2016d1ed18f4fbd1fca313
intwocave <intwocave@gmail.com>
2025-08-13 13:25:14 +0900

    feat: add basic main.js for feature/ranking

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 import express from 'express';
+
 import GroupRouter from "./src/router/group-router.js";
+import rankingRouter from 'src/router/rankingRouter.js';
 
-
-
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
+
+function requestLogger(req, _, next) {
+    console.log(`[${req.method}] ${req.originalUrl}`);
+    next();
+}
+
+
+app.use(requestLogger);
 app.use('/groups', GroupRouter);
+app.use('/groups/:groupId/rank', rankingRouter);
 
 
-app.listen(3000, () => {
-    'app is listen at http://localhost:3000'
-})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}..`));
