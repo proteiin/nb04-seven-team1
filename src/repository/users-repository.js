e9ac69f16678f76c0eec60fa5b56eee4529f66ae
InsipidPie1229 <ghdwnsaos@gmail.com>
2025-08-13 17:47:47 +0900

    feat: Add repository method for group participation

--- src/repository/users-repository.js
+++ src/repository/users-repository.js
@@ -11,4 +11,21 @@ export class UsersRepository {
     });
     return userNickname;
   };
+
+  // 사용자의 그룹 참여 시 그룹의 상세 정보 반환
+  participationInTheGroup = async (groupId) => {
+    const participation = await prisma.group.findUnique({
+      where: { id: groupId },
+      include: {
+        tags: true,
+        user: {
+          select: {
+            id: true,
+            nickname: true,
+          },
+        },
+      },
+    });
+    return participation;
+  };
 }
