620806c30b9015a9f653f1e0f7e0fab130e088e9
InsipidPie1229 <ghdwnsaos@gmail.com>
2025-08-14 14:49:01 +0900

    refactor: Change middleware function to class

--- src/middleware/user-validation-middleware.js
+++ src/middleware/user-validation-middleware.js
@@ -1,78 +1,81 @@
 import { UsersRepository } from '../repository/user-repository.js';
 
-const usersRepository = new UsersRepository();
-
-export async function checkDuplicateNickname(req, res, next) {
-  try {
-    const { groupId } = req.params;
-    const { nickname } = req.body;
-
-    const numericGroupId = Number(groupId);
-    if (Number.isNaN(numericGroupId)) {
-      return res.status(400).json({
-        path: 'groupId',
-        message: 'group_id is unvalidated',
-      });
-    }
-
-    const nicknameDuplicated = await usersRepository.findByNickname(
-      nickname,
-      numericGroupId,
-    );
-
-    if (nicknameDuplicated) {
-      return res.status(409).json({
-        path: 'nickname',
-        message: 'nickname is duplicated',
-      });
-    }
-    next();
-  } catch (error) {
-    next(error);
+export class UserValidator {
+  constructor() {
+    this.usersRepository = new UsersRepository();
   }
-}
+  checkDuplicateNickname = async (req, res, next) => {
+    try {
+      const { groupId } = req.params;
+      const { nickname } = req.body;
 
-export async function checkNullNickname(req, res, next) {
-  try {
-    const { nickname } = req.body;
+      const numericGroupId = Number(groupId);
+      if (Number.isNaN(numericGroupId)) {
+        return res.status(400).json({
+          path: 'groupId',
+          message: 'group_id is unvalidated',
+        });
+      }
 
-    if (!nickname) {
-      return res.status(400).json({
-        path: 'nickname',
-        message: 'nickname is required',
-      });
-    }
-    if (nickname.trim().length === 0) {
-      return res.status(400).json({
-        path: 'nickname',
-        message: 'nickname can not be just blank',
-      });
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
-}
+      const nicknameDuplicated = await usersRepository.findByNickname(
+        nickname,
+        numericGroupId,
+      );
 
-// 비밀번호 빈 칸, 공백 확인
-export async function checkNullPassword(req, res, next) {
-  try {
-    const { password } = req.body;
+      if (nicknameDuplicated) {
+        return res.status(409).json({
+          path: 'nickname',
+          message: 'nickname is duplicated',
+        });
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 
-    if (!password) {
-      return res.status(400).json({
-        path: 'password',
-        message: 'password is required',
-      });
+  checkNullNickname = async (req, res, next) => {
+    try {
+      const { nickname } = req.body;
+
+      if (!nickname) {
+        return res.status(400).json({
+          path: 'nickname',
+          message: 'nickname is required',
+        });
+      }
+      if (nickname.trim().length === 0) {
+        return res.status(400).json({
+          path: 'nickname',
+          message: 'nickname can not be just blank',
+        });
+      }
+      next();
+    } catch (error) {
+      next(error);
     }
-    if (password.trim().length === 0) {
-      return res.status(400).json({
-        path: 'password',
-        message: 'password can not be just blank',
-      });
+  };
+
+  // 비밀번호 빈 칸, 공백 확인
+  checkNullPassword = async (req, res, next) => {
+    try {
+      const { password } = req.body;
+
+      if (!password) {
+        return res.status(400).json({
+          path: 'password',
+          message: 'password is required',
+        });
+      }
+      if (password.trim().length === 0) {
+        return res.status(400).json({
+          path: 'password',
+          message: 'password can not be just blank',
+        });
+      }
+      next();
+    } catch (error) {
+      next(error);
     }
-    next();
-  } catch (error) {
-    next(error);
-  }
+  };
 }
