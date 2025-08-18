0bd9c0f3591a68cde09a8438e4bad9fe864d9f4c
InsipidPie1229 <ghdwnsaos@gmail.com>
2025-08-18 13:28:35 +0900

    feat: Add service when users join in a group

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -4,7 +4,17 @@ class UserService {
   constructor() {
     this.userRepository = new UserRepository();
   }
-  addParticipantToGroup = async (nickname, password, groupId) => {};
+  addParticipantToGroup = async (nickname, password, groupId) => {
+    const updatedGroup = await prisma.$transaction(async (tx) => {
+      await this.userRepository.joinGroup(
+        { group_id: groupId, nickname, password },
+        tx,
+      );
+      const result = await this.userRepository.incrementGroupUser(groupId, tx);
+      return result;
+    });
+    return updatedGroup;
+  };
 
   leaveParticipantFromGroup = async (nickname, password, groupId) => {};
 }
