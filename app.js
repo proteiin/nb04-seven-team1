dc3e6a111b3dd50c31a69e650005871e13741dd5
Jerang2 <wpckd324@gmail.com>
2025-08-18 15:23:42 +0900

    feat: Add app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+//import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "./generated/prisma/index.js";
+
+import { RecordsRepository } from './src/repository/records.repository.js';
+import { RecordsService } from './src/service/records.service.js';
+import { RecordsController } from './src/controller/records.controller.js';
+
+import recordsRouter from './src/router/records.router.js';
+
+const app = express();
+const PORT = 3000;
+
+const prisma = new PrismaClient();
+const recordsRepository = new RecordsRepository(prisma);
+const recordsService = new RecordsService(recordsRepository);
+const recordsController = new RecordsController(recordsService);
+
+app.use(express.json());
+app.use('/api', recordsRouter(recordsController));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({ message: err.message || '인터넷 에러' });
+});
+
+app.listen(PORT, () => {
+    console.log(`${PORT}에 서버를 시작합니다.`);
+})
+
+process.on('beforeExit', async () => {
+    await prisma.$disconnect();
+});
