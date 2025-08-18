39b813f34b25449de698a442a2bc0ba5f967a408
Jerang2 <wpckd324@gmail.com>
2025-08-18 15:20:25 +0900

    feat: Add records.controller

---
+++ src/controller/records.controller.js
@@ -0,0 +1,65 @@
+import { RecordsService } from '../service/records.service.js'; // 서비스 경로
+
+export class RecordsController {
+    constructor(recordsService) {
+        this.recordsService = recordsService;
+    }
+    createRecord = async (req, res, next) => {
+        try {
+            const { groupId } = req.params;
+            const { nickname, password, exerciseType, description, time, distance, photos} = req.body
+            ;
+
+            if (!nickname || !password || !exerciseType || !time || !distance) {
+                return res.status(400).json({ message: '필수 값을 모두 입력해주세요.'});
+            }
+            
+            const newRecord = await this.recordsService.createRecord(
+                +groupId,
+                nickname,
+                exerciseType,
+                description,
+                time,
+                distance,
+                password,                
+            );
+
+            return res.status(201).json({ data: newRecord });
+        }   catch(error) {
+            next(error);
+        }
+    };
+
+    findAllRecords = async (req, res, next) => {
+        try {
+            const { groupId } = req.params;
+            const { sort, search, page } = req.query;
+            const records = await this.recordsService.findAllRecords(+groupId, sort, search, page);
+
+            return res.status(200).json({ data: records });
+        }   catch(error) {
+            next(error);
+        }
+    }
+
+        findRecordsRanking = async (req, res, next) => {
+            try {
+                const { groupId } = req.params;
+                const { period } = req.query;
+                const ranking = await this.recordsService.findRecordsRanking(+groupId, period);
+                return res.status(200).json({ data: ranking });
+            } catch(error) {
+                next(error);
+            }
+        }
+        
+        findRecordById = async (req, res, next) => {
+            try {
+                const { groupId, recordId } = req.params;
+                const record = await this.recordsService.findRecordById(+groupId, +recordId);
+                return res.status(200).json({ data: record });
+            } catch(error) {
+                next(error);
+            }
+        };
+    }
