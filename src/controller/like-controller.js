5acaa874e0b6a79548526c34a124fe31cd3d9ac8
Hoon <dlgnswp21@gmail.com>
2025-08-18 17:47:41 +0900

    feat: add like controller

---
+++ src/controller/like-controller.js
@@ -0,0 +1,27 @@
+import LikeService from '../service/like-service.js';
+
+class LikeController {
+  static async addLike(req, res, next) {
+    try {
+      const groupId = parseInt(req.params.groupId, 10);
+
+      const likecount = await LikeService.addLike(groupId);
+      res.status(201).json({ message: '좋아요 추가 완료', likecount });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async removeLike(req, res, next) {
+    try {
+      const groupId = parseInt(req.params.groupId, 10);
+
+      const likecount = await LikeService.removeLike(groupId);
+      res.status(200).json({ message: '좋아요 취소 완료', likecount });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default LikeController;
