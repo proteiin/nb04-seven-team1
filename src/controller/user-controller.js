f91fb9c9d11fae58503dd67569bc0a2a71e8fe95
InsipidPie1229 <ghdwnsaos@gmail.com>
2025-08-18 17:15:08 +0900

    refactor: Convert user controller to a class

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,35 +1,41 @@
 import userService from '../service/user-service.js';
 
-export const groupParticipation = async (req, res, next) => {
-  try {
-    const { groupId } = req.params;
-    const { nickname, password } = req.body;
-
-    const result = await userService.addParticipantToGroup(
-      nickname,
-      password,
-      groupId,
-    );
-
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+export class UserController {
+  constructor(userService) {
+    this.userService = userService;
   }
-};
 
-export const groupLeave = async (req, res, next) => {
-  try {
-    const { groupId } = req.params;
-    const { nickname, password } = req.body;
+  groupParticipation = async (req, res, next) => {
+    try {
+      const { groupId } = req.params;
+      const { nickname, password } = req.body;
 
-    const result = await userService.leaveParticipantFromGroup(
-      nickname,
-      password,
-      groupId,
-    );
+      const result = await this.userService.addParticipantToGroup(
+        nickname,
+        password,
+        Number(groupId),
+      );
 
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-};
+      res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  groupLeave = async (req, res, next) => {
+    try {
+      const { groupId } = req.params;
+      const { nickname, password } = req.body;
+
+      const result = await this.userService.leaveParticipantFromGroup(
+        nickname,
+        password,
+        Number(groupId),
+      );
+
+      res.status(204).send();
+    } catch (error) {
+      next(error);
+    }
+  };
+}
