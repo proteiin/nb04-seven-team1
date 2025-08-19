827b575b266485ae7acc97b05c5a75949e19ec80
Hoon <dlgnswp21@gmail.com>
2025-08-19 16:00:21 +0900

    feat: add tag service

---
+++ src/service/tag-service.js
@@ -0,0 +1,22 @@
+import TagRepogitory from '../repository/tag-repository.js';
+
+class TagService {
+  async getTags(page, limit, order, search) {
+    const where = {
+      group: { user: { nickname: { contains: search, mode: 'insensitive' } } },
+    };
+
+    const skip = (page - 1) * limit;
+    const take = limit;
+
+    return await TagRepogitory.getTags(where, order, skip, take);
+  }
+
+  async getTagId(tagId) {
+    const where = { id: tagId };
+
+    return TagRepogitory.getTagId(where);
+  }
+}
+
+export default new TagService();
