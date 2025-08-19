6e39d596fc4d635977a1b35e94d16fcd7433a99c
Hoon <dlgnswp21@gmail.com>
2025-08-19 15:43:51 +0900

    feat: add tag repository

---
+++ src/repository/tag-repository.js
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+class TagRepogitory {
+  async getTags(where, order, skip, take) {
+    return await prisma.tag.findMany({
+      where,
+      orderBy: { created_at: order },
+      skip,
+      take,
+      select: {
+        id: true,
+        name: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
+  }
+
+  async getTagId(where) {
+    return await prisma.tag.findUnique({
+      where,
+      select: {
+        id: true,
+        name: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
+  }
+}
+
+export default new TagRepogitory();
