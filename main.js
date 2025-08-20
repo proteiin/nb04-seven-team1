843d90dde56c24e54ef9f27c4e6ee7579596ef97
Son JuneYeong <sonj0407@naver.com>
2025-08-20 11:21:00 +0900

    fix: fixed merge errors

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-// import express from 'express';
-// import GroupRouter from "./src/router/group-router.js";
-
-
-
-
-// const app = express();
-
-// app.use(express.json());
-
-// app.use('/groups', GroupRouter);
-
-
-// app.listen(3000, () => {
-//     console.log('app is listen at http://localhost:3000')
-// })
-
-
 import express from 'express';
 import cors from 'cors';
 
