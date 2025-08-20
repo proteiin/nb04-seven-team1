45be665258e232a4601d24c6e2d2d81d0b9ed97e
Son JuneYeong <sonj0407@naver.com>
2025-08-20 11:31:49 +0900

    fix: fixed main file

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
-import GroupRouter from "./src/router/group-router.js";
+// import GroupRouter from "./src/router/group-router.js";
 import RankingRouter from './src/router/RankingRouter.js';
 
 const PORT = process.env.PORT || 3000;
