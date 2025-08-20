d41f9f08552c646b7d31a7cf783f827b81902243
Son JuneYeong <sonj0407@naver.com>
2025-08-20 11:31:49 +0900

    fix: fixed errors in merge

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ import * as dotenv from 'dotenv'; // 환경변수를 위한 라이브러리
 import RankingRouter from './src/router/RankingRouter.js';
 import userRouter from './src/router/user-router.js';
 
+import userRouter from './src/router/user-router.js';
+
+
 dotenv.config();
 
+
 const PORT = process.env.PORT || 3000;
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 function requestLogger(req, _, next) {
   console.log(`[${req.method}] ${req.originalUrl}`);
   next();
 }
-
 app.use(requestLogger);
 
 const rankingRouter = new RankingRouter();
@@ -29,4 +35,25 @@ app.use('/groups/:groupId/participants', userRouter);
 
 
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}..`));
+
+// 전역 에러 핸들러 미들웨어
+// 반드시 모든 라우터 뒤에 위치해야 합니다.
+app.use((err, req, res, next) => {
+  console.error(err); // 에러 로그
+
+  const statusCode = err.statusCode || 500; // 에러 객체의 상태코드가 없으면 500을 기본값으로 사용
+
+  const message = err.message || 'unexpected server error';
+  const path = err.path || 'unknown';
+
+  res.status(statusCode).json({
+    path: path,
+    message: message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
