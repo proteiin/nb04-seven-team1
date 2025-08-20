df32836de779d025fd649034ee439e17f220d5f0
Son JuneYeong <sonj0407@naver.com>
2025-08-20 16:00:46 +0900

    "fix: annotated useless codes"

--- src/router/RankingRouter.js
+++ src/router/RankingRouter.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import RankingController from '../controller/RankingController.js';
+// import express from 'express';
+// import RankingController from '../controller/RankingController.js';
 
-export default class RankingRouter {
-  constructor() {
-    this.router = express.Router({ mergeParams: true });
-    this.rankingController = new RankingController();
+// export default class RankingRouter {
+//   constructor() {
+//     this.router = express.Router({ mergeParams: true });
+//     this.rankingController = new RankingController();
 
-    this.initializeRouter();
-  }
+//     this.initializeRouter();
+//   }
 
-  initializeRouter() {
-    this.router
-      .route('/')
-      .get(this.rankingController.getRanking.bind(this.rankingController));
-  }
+//   initializeRouter() {
+//     this.router
+//       .route('/')
+//       .get(this.rankingController.getRanking.bind(this.rankingController));
+//   }
 
-  getRouter() {
-    return this.router;
-  }
-}
+//   getRouter() {
+//     return this.router;
+//   }
+// }
