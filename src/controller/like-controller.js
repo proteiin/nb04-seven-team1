88bc5bb01456ecb88201e427cc4e80b45957df76
jaehoon21 <dlgnswp21@gmail.com>
2025-08-21 14:11:59 +0900

    Update src/controller/like-controller.js
    Co-authored-by: JoonKi HONG <118425817+InsipidPie1229@users.noreply.github.com>

--- src/controller/like-controller.js
+++ src/controller/like-controller.js
@@ -3,7 +3,7 @@ import LikeService from '../service/like-service.js';
 class LikeController {
   async addLike(req, res, next) {
     try {
-      const groupId = parseInt(req.params.groupId, 10);
+      const { groupId } = req;
 
       const likecount = await LikeService.addLike(groupId);
       res.status(200).json({ message: '좋아요 추가 완료', likecount });
