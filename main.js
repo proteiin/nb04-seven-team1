1c0bd023a36cb597bd1a2ffa5f6df51becabf92a
InsipidPie1229 <ghdwnsaos@gmail.com>
2025-08-22 13:34:44 +0900

    refactor: Integrate user feature with DI container

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv'; // 환경변수를 위한 라이브러리
 
+import container from './src/container.js';
+
 import GroupRouter from './src/router/group-router.js';
 import RankingRouter from './src/router/RankingRouter.js';
-import userRouter from './src/router/user-router.js';
+import userRouter from './src/router/user-router.js'; // 라우터 함수를 가져와야 합니다.
 import ImageRouter from './src/router/ImageRouter.js';
 import recordsRouter from './src/router/records.router.js';
+
+// 컨테이너로 가져오시면 지워도 될 것 같습니다.
 import { RecordsRepository } from './src/repository/records.repository.js';
 import { RecordsService } from './src/service/records.service.js';
 import { RecordsController } from './src/controller/records.controller.js';
@@ -18,10 +22,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
 app.use(cors());
-
 
 function requestLogger(req, _, next) {
   console.log(`[${req.method}] ${req.originalUrl}`);
@@ -29,15 +31,22 @@ function requestLogger(req, _, next) {
 }
 app.use(requestLogger);
 
+// 역시 컨테이너를 사용하시면 컨테이너에서 조립을 하시면 될 것 같습니다.
 const rankingRouter = new RankingRouter();
 const imageRouter = new ImageRouter();
 const recordsRepository = new RecordsRepository();
 const recordsService = new RecordsService(recordsRepository);
 const recordsController = new RecordsController(recordsService);
-  
+
+// 컨테이너에서 필요에 따라 꺼내서 라우터에 전달합니다.
+const { userController, userValidator } = container;
+
 app.use('/groups', GroupRouter);
 app.use('/groups/:groupId/rank', rankingRouter.getRouter());
-app.use('/groups/:groupId/participants', userRouter);
+app.use(
+  '/groups/:groupId/participants',
+  userRouter(userController, userValidator),
+);
 app.use('/images', imageRouter.getRouter());
 app.use('/api', recordsRouter(recordsController));
 app.use('/tags', tagRouter);
@@ -54,7 +63,7 @@ app.use((err, req, res, next) => {
 
   res.status(statusCode).json({
     path,
-    message
+    message,
   });
 });
 
