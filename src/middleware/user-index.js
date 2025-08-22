9a40f69daef29c93dbdad6461f5b1c29aa33116e
InsipidPie1229 <ghdwnsaos@gmail.com>
2025-08-22 10:50:40 +0900

    chore: Renam user-index.js files to index.js

--- src/middleware/user-index.js
+++ src/middleware/user-index.js
@@ -1,4 +1,4 @@
 import { UserValidator } from './user-validation-middleware.js';
-import { userRepository } from '../repository/user-index.js';
+import { userRepository } from '../repository/index.js';
 
 export const userValidator = new UserValidator(userRepository);
