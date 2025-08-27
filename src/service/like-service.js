4e44ea9d48a4708b0193f6a9fa9fbe006281fe4e
Hoon <dlgnswp21@gmail.com>
2025-08-27 14:43:23 +0900

    feat: add like badge

--- src/service/like-service.js
+++ src/service/like-service.js
@@ -1,14 +1,63 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export class LikeService {
-  constructor(likeRepository) {
+  constructor(likeRepository, prisma) {
     this.likeRepository = likeRepository;
+    this.prisma = prisma;
   }
 
   async addLike(groupId) {
-    return await this.likeRepository.updateLikeCount(groupId, 1);
+    const { like_count } = await this.likeRepository.updateLikeCount(
+      groupId,
+      1,
+    );
+
+    if (like_count >= 100) {
+      const badge = await prisma.badge.findFirst({
+        where: {
+          code: 'LIKE_100',
+          group_id: groupId,
+        },
+      });
+
+      if (!badge) {
+        await prisma.badge.create({
+          data: {
+            code: 'LIKE_100',
+            group_id: groupId,
+          },
+        });
+      }
+    }
   }
 
   async removeLike(groupId) {
-    return await this.likeRepository.updateLikeCount(groupId, -1);
+    const { like_count } = await this.likeRepository.updateLikeCount(
+      groupId,
+      -1,
+    );
+
+    if (like_count < 100) {
+      const badge = await prisma.badge.findFirst({
+        where: {
+          code: 'LIKE_100',
+          group_id: groupId,
+        },
+      });
+
+      if (badge) {
+        await prisma.badge.delete({
+          where: {
+            code_group_id: {
+              code: 'LIKE_100',
+              group_id: groupId,
+            },
+          },
+        });
+      }
+    }
   }
 }
 
