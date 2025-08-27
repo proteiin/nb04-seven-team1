9a9914e4b5feddfdbb5cae99a9d2186f2e18d9fc
Hoon <dlgnswp21@gmail.com>
2025-08-27 16:50:02 +0900

    refactor: badge storage to string format

--- src/service/like-service.js
+++ src/service/like-service.js
@@ -3,12 +3,65 @@ export class LikeService {
     this.likeRepository = likeRepository;
     this.prisma = prisma;
   }
+  addLike = async (groupId) => {
+    const { like_count } = await this.likeRepository.updateLikeCount(groupId, 1);
 
+    if (like_count >= 100) {
+      const group = await this.prisma.group.findUnique({
+        where: { id: groupId },
+      });
+
+     
+      if (!group.badges.includes('LIKE_100')) {
+        await this.prisma.group.update({
+          where: { id: groupId },
+          data: {
+            badges: [...group.badges, 'LIKE_100'], 
+          },
+        });
+      }
+    }
+  };
+
+  
+  removeLike = async (groupId) => {
+    const { like_count } = await this.likeRepository.updateLikeCount(groupId, -1);
+
+    if (like_count < 100) {
+      const group = await this.prisma.group.findUnique({
+        where: { id: groupId },
+      });
+
+      if (group.badges.includes('LIKE_100')) {
+        const updatedBadges = group.badges.filter(badge => badge !== 'LIKE_100');
+        
+        await this.prisma.group.update({
+          where: { id: groupId },
+          data: {
+            badges: updatedBadges,
+          },
+        });
+      }
+    }
+  };
+}
+
+  
+/* class LikeService {
+  async addLike(groupId) {
+    return await LikeRepository.updateLikeCount(groupId, 1);
+  }
+
+  async removeLike(groupId) {
+    return await LikeRepository.updateLikeCount(groupId, -1);
+  }
+}
+
+export default new LikeService(); */
+
+/*
  addLike = async(groupId) =>{
-    const { like_count } = await this.likeRepository.updateLikeCount(
-      groupId,
-      1,
-    );
+    const { like_count } = await this.likeRepository.updateLikeCount(groupId, 1);
 
     if (like_count >= 100) {
       const badge = await this.prisma.badge.findFirst({
@@ -55,16 +108,4 @@ export class LikeService {
       }
     }
   }
-}
-
-/* class LikeService {
-  async addLike(groupId) {
-    return await LikeRepository.updateLikeCount(groupId, 1);
-  }
-
-  async removeLike(groupId) {
-    return await LikeRepository.updateLikeCount(groupId, -1);
-  }
-}
-
-export default new LikeService(); */
+    */
