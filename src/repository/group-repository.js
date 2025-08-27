8091f426e8eef430aff1785bb855a9aada6e0830
Hoon <dlgnswp21@gmail.com>
2025-08-27 16:50:02 +0900

    refactor: badge storage to string format

--- src/repository/group-repository.js
+++ src/repository/group-repository.js
@@ -57,7 +57,6 @@ export class GroupRepository{
         where: { id: groupId },
         include: {
             tags: true,
-            badges: true,
             user: {
             select: {
                 id: true,
@@ -70,7 +69,7 @@ export class GroupRepository{
         },
         });
 
-        group.badges = (group.badges || []).map((badge) => badge.code);
+        group.badges = group.badges || []
 
         return group;
     };
